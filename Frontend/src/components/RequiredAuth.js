67b04740b387b7ddc05cb1c461da566aaefcd0c8
Manh Phan <ptm22022002@gmail.com>
2024-06-12 17:14:29 +0700

    false

--- Frontend/src/components/RequiredAuth.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useAuthContext } from "../context/AuthContext";
-
-export default function RequiredAuth(props) {
-    const { children } = props
-    const { isAuthenticated } = useAuthContext()
-
-    return isAuthenticated ? children : <Navigate to="/login" />
-}
